16d2b069119379c737a851592ca2dd3b718d3e37
Nguyen Thê-viet <theviet.pham@gmail.com>
2024-06-07 15:31:06 +0200

    ajout de commentaires

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const userRoutes = require('./routes/user');
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .then(() => console.log(" Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
