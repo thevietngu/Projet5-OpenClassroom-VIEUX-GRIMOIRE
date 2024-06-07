f2291b392b7d5b2425a0d6fd909736823c414ca8
Nguyen Thê-viet <theviet.pham@gmail.com>
2024-06-07 16:17:15 +0200

    ajout de commentaires

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const path = require('path');
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
 
+// Connexion à la base de données
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(" Connexion à MongoDB réussie !"))
@@ -29,10 +30,15 @@ app.use((req, res, next) => {
   next();
 });
 
+//permet de récupérer les données envoyées dans le corps d'une requête POST sous forme d'un objet JavaScript accessible via req.body
 app.use(bodyParser.json());
 
+// définition Routes pour Book et User
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+
+
+// Configuration Express pour servir des fichiers statiques (ici, les images) à partir du répertoire /images
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
